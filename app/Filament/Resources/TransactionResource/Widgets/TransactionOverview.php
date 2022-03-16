f940b9406694b5e5302f19f2b2f7003463fb33a4
hellohendri <hendrinarendra@gmail.com>
2022-03-16 08:12:34 +0700

    add idr currency to cards

--- app/Filament/Resources/TransactionResource/Widgets/TransactionOverview.php
+++ app/Filament/Resources/TransactionResource/Widgets/TransactionOverview.php
@@ -9,8 +9,10 @@ use Filament\Widgets\StatsOverviewWidget\Card;
 
 class TransactionOverview extends BaseWidget
 {
+
     protected function getCards(): array
     {
+
         $income = Transaction::where('transaction_type', 1)
             ->sum('total');
 
@@ -19,16 +21,20 @@ class TransactionOverview extends BaseWidget
 
         $balance = $income - $expense;
 
+        $incomeWithCurrency = "Rp " . number_format($income, 2, ',', '.');
+        $expenseWithCurrency = "Rp " . number_format($expense, 2, ',', '.');
+        $balanceWithCurrency = "Rp " . number_format($balance, 2, ',', '.');
+
         return [
-            Card::make('Total Pemasukan', $income)
+            Card::make('Total Pemasukan', $incomeWithCurrency)
                 ->description('Total Pemasukan Dari Semua Jenis Transaksi')
                 ->descriptionIcon('heroicon-s-trending-up')
                 ->color('success'),
-            Card::make('Total Pengeluaran', $income)
+            Card::make('Total Pengeluaran', $expenseWithCurrency)
                 ->description('Total Pengeluaran Dari Semua Jenis Transaksi')
                 ->descriptionIcon('heroicon-s-trending-down')
                 ->color('danger'),
-            Card::make('Saldo', $balance)
+            Card::make('Saldo', $balanceWithCurrency)
                 ->description('Jumlah Saldo Saat Ini')
                 ->descriptionIcon('heroicon-s-credit-card')
                 ->color('warning'),
